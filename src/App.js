248499e10f94c21333bbafc639b6e6e95070c042
s24530 <s24530@pjwstk.edu.pl>
2023-12-15 15:22:24 +0100

    done app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [userData, setUserData] = useState(null);
+
+  const fetchRandomUser = async () => {
+    try {
+      const response = await fetch("https://randomuser.me/api/");
+      const data = await response.json();
+
+      setUserData(data.results[0]);
+    } catch (error) {
+      console.error("Błąd podczas pobierania danych:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchRandomUser();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Losowe Dane Użytkownika</h1>
+
+      <button onClick={fetchRandomUser}>Losuj kolejne dane</button>
+
+      {userData && (
+        <div className="user-data">
+          <img src={userData.picture.large} alt="User" />
+          <p>
+            <strong>Imię:</strong> {userData.name.first}
+          </p>
+          <p>
+            <strong>Nazwisko:</strong> {userData.name.last}
+          </p>
+          <p>
+            <strong>Email:</strong> {userData.email}
+          </p>
+          <p>
+            <strong>Miasto:</strong> {userData.location.city}
+          </p>
+        </div>
+      )}
     </div>
   );
 }
